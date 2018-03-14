6306cc90896a3a642121bd8dabc6df16b6be3f13
Jaclyn Mullin <jaclyn.mullin@gmail.com>
2018-03-14 18:06:29 -0400

    all lowercase

--- cleandata.py
+++ cleandata.py
@@ -1,7 +1,7 @@
-from datamerge.py import *
+from datamerge import *
+
 
 # This creates a bool column to match search term with search term being male. How many male medalists?
-
 
 def create_bool_field_from_search_term(data_sample, search_term):
     new_array = []
@@ -26,6 +26,6 @@ def filter_col_by_bool(data_sample, col):
 
     return matches_search_term
 
-new_csv = create_bool_field_from_search_term(data_from_csv, "Men")
-number_of_male_medalists = number_of_records(filter_col_by_bool(new_csv, 9))
-print("Male Medal Winners": number_of_male_medalists(new_csv))
+new_csv = create_bool_field_from_search_term(results, "men")
+number_of_male_medalists = results(filter_col_by_bool(new_csv, 9))
+print("Male Medal Winners:" number_of_male_medalists(new_csv))
--- datamerge.py
+++ datamerge.py
@@ -12,8 +12,13 @@ results = pd.DataFrame([])
 
 for counter, current_file in enumerate(glob.glob("*.csv")):
     namedf = pd.read_csv(current_file, encoding='iso-8859-1', usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-    print(namedf[['City', 'Sport', 'Discipline', 'Athlete', 'Country', 'Gender', 'Event', 'Medal']].applymap(str.lower))
-    results = pd.concat([results, namedf])
+    namedf['Country'] = namedf.Country.astype(str)
+    x = namedf[['City', 'Sport', 'Discipline', 'Athlete', 'Country', 'Gender', 'Event', 'Medal']].applymap(str.lower)
+    x['Year'] = namedf.Year
+
+    print(x)
+
+    results = pd.concat([results, x])
 
 results.to_csv('/Users/jaclyn/Desktop/CodeLouisville/Python_Data/olympics_project/output_data/totalresults.csv',
                index=None,
