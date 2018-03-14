b7d40958ee16e67592b542c6fa21e2ab57558cde
Jaclyn Mullin <jaclyn.mullin@gmail.com>
2018-03-14 14:40:55 -0400

    working kinks in data still

--- datamerge.py
+++ datamerge.py
@@ -12,10 +12,10 @@ results = pd.DataFrame([])
 
 for counter, current_file in enumerate(glob.glob("*.csv")):
     namedf = pd.read_csv(current_file, encoding='iso-8859-1', usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
-    namedf["lowercase"] = namedf[['City']].apply(lambda name: name.str.lower())
-    print(namedf)
+    print(namedf[['City', 'Sport', 'Discipline', 'Athlete', 'Country', 'Gender', 'Event', 'Medal']].applymap(str.lower))
     results = pd.concat([results, namedf])
 
 results.to_csv('/Users/jaclyn/Desktop/CodeLouisville/Python_Data/olympics_project/output_data/totalresults.csv',
                index=None,
                )
+
